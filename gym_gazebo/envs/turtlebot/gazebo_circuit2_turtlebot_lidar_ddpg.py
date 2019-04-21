2e85eba17a53be18bbc8ae03fab3c3319fd7bee2
Matt Chan <matthew.a.chan@gmail.com>
2019-04-21 16:39:11 -0400

    Update environment

--- gym_gazebo/envs/turtlebot/gazebo_circuit2_turtlebot_lidar_ddpg.py
+++ gym_gazebo/envs/turtlebot/gazebo_circuit2_turtlebot_lidar_ddpg.py
@@ -29,8 +29,10 @@ class GazeboCircuit2TurtlebotLidarDdpgEnv(gazebo_env.GazeboEnv):
         self.reward_range = (-np.inf, np.inf)
         self.prev_action = [0] * 2
 
-        self.goal = Pose()
+        self.prev_dist = np.inf 
+
         # Goal position
+        self.goal = Pose()
         self.goal.position.x = -6
         # Identity quaternion (no rotation)
         self.goal.orientation.x = 1
@@ -55,9 +57,14 @@ class GazeboCircuit2TurtlebotLidarDdpgEnv(gazebo_env.GazeboEnv):
             if (min_range > data.ranges[i] > 0):
                 done = True
 
+            # Normalize observation between 0 and 1
+            normalized = (data.ranges[i] - data.range_min) / (data.range_max - data.range_min)
+
             # Store new minimum
-            if (data.ranges[i] < discretized_ranges[bucket]):
-                discretized_ranges[bucket] = data.ranges[i]
+            if (normalized < discretized_ranges[bucket]):
+                discretized_ranges[bucket] = normalized
+
+        print discretized_ranges
 
         return discretized_ranges,done
 
@@ -73,10 +80,11 @@ class GazeboCircuit2TurtlebotLidarDdpgEnv(gazebo_env.GazeboEnv):
             print ("/gazebo/unpause_physics service call failed")
 
         # action has linear and angular vel components
-        # lin_vel = action[0]
-        lin_vel = 0.2
-        ang_vel =0.2
-        # ang_vel = action[1]
+
+        print "ACTION"
+        print action
+        lin_vel = action[0][0]
+        ang_vel = action[0][1]
 
         max_ang_speed = 0.3
         # ang_vel = (action-10)*max_ang_speed*0.1 #from (-0.33 to + 0.33)
@@ -111,14 +119,29 @@ class GazeboCircuit2TurtlebotLidarDdpgEnv(gazebo_env.GazeboEnv):
 
         state,done = self.calculate_observation(data)
 
+
+        # Build state
+        dist, angle = self.get_polar_coords(odom)
+        self.prev_dist = dist
+        state = np.concatenate((np.asarray(state), np.asarray(action), dist, angle), axis=None)
+
+        self.prev_action = action
+
+        # Compute rewards
         if not done:
             # Straight reward = 5, Max angle reward = 0.5
-            reward = round(15*(max_ang_speed - abs(ang_vel) +0.0335), 2)
+            reward = 1 * (self.prev_dist - dist)
+            # reward = round(15*(max_ang_speed - abs(ang_vel) +0.0335), 2)
             # print ("Action : "+str(action)+" Ang_vel : "+str(ang_vel)+" reward="+str(reward))
         else:
+            # Collision reward
             reward = -200
 
+        print "REWARD: " + str(reward)
 
+        return np.asarray(state), reward, done, {}
+
+    def get_polar_coords(self, odom):
         # Compute polar coordinates
 
         # Get vector from robot to the goal
@@ -127,16 +150,14 @@ class GazeboCircuit2TurtlebotLidarDdpgEnv(gazebo_env.GazeboEnv):
         goal_pos = [self.goal.position.x, self.goal.position.y]
         to_goal = np.subtract(goal_pos, robot_pos)
         dist = np.linalg.norm(to_goal)
-        print "DIST"
-        print dist
+        print "DIST: " + str(dist)
 
         # Get the robot's forward vector
         orientation = odom.pose.pose.orientation
         quaternion = [orientation.x, orientation.y, orientation.z, orientation.w]
         euler = tf.transformations.euler_from_quaternion(quaternion)
         yaw = euler[2]
-        print "YAW"
-        print yaw
+        print "YAW: " + str(yaw)
 
         forward = [1, 0]
         rot_mat = np.array([[np.cos(yaw), -np.sin(yaw)],
@@ -146,20 +167,10 @@ class GazeboCircuit2TurtlebotLidarDdpgEnv(gazebo_env.GazeboEnv):
 
         # Get the angle 
         angle = np.arccos(robot_forward.dot(to_goal) / (1 * dist))
-        print "ANGLE (in radians)"
-        print angle
-        
+        print "ANGLE (in radians): " + str(angle)
 
-        # Build state
-        print len(state)
-        print len(action)
-        state = np.concatenate((np.asarray(state), np.asarray(action), dist, angle), axis=None)
-        print "STATE LEN"
-        print len(state)
+        return dist, angle
 
-        self.prev_action = action
-
-        return np.asarray(state), reward, done, {}
 
     def reset(self):
         # Resets the state of the environment and returns an initial observation.
@@ -169,6 +180,14 @@ class GazeboCircuit2TurtlebotLidarDdpgEnv(gazebo_env.GazeboEnv):
             self.reset_proxy()
         except (rospy.ServiceException) as e:
             print ("/gazebo/reset_simulation service call failed")
+
+        # Read odometry data
+        odom = None
+        while odom is None:
+            try:
+                odom = rospy.wait_for_message('/odom', Odometry, timeout=5)
+            except:
+                pass
 
         # Reset odometry
         reset_odom = rospy.Publisher('/mobile_base/commands/reset_odometry', Empty, queue_size=10)
@@ -199,5 +218,7 @@ class GazeboCircuit2TurtlebotLidarDdpgEnv(gazebo_env.GazeboEnv):
             print ("/gazebo/pause_physics service call failed")
 
         state,done = self.calculate_observation(data)
+        dist, angle = self.get_polar_coords(odom)
+        state = np.concatenate((np.asarray(state), np.asarray(self.prev_action), dist, angle), axis=None)
 
-        return np.concatenate((np.asarray(state), self.prev_action), axis=None)
+        return np.asarray(state)
