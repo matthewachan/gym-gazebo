2aa31bb9f59739387aaf78ed7ab88c117e9a09ed
jhu20 <jh3916@columbia.edu>
2019-04-30 17:05:09 -0400

    Revert "ss"
    This reverts commit 351954db31594d26203330247609c3f73116b031.

--- gym_gazebo/envs/turtlebot/gazebo_circuit2_turtlebot_lidar_ddpg.py
+++ gym_gazebo/envs/turtlebot/gazebo_circuit2_turtlebot_lidar_ddpg.py
@@ -25,8 +25,7 @@ class GazeboCircuit2TurtlebotLidarDdpgEnv(gazebo_env.GazeboEnv):
         # Specify the map to load
         #gazebo_env.GazeboEnv.__init__(self, "GazeboCircuit2TurtlebotLidar_v0.launch")
         #gazebo_env.GazeboEnv.__init__(self, "GazeboDebug_v0.launch")
-        #gazebo_env.GazeboEnv.__init__(self, "GazeboEnv1.launch")
-        gazebo_env.GazeboEnv.__init__(self, "GazeboEnv2.launch")
+        gazebo_env.GazeboEnv.__init__(self, "GazeboEnv1.launch")
 
         self.vel_pub = rospy.Publisher('/mobile_base/commands/velocity', Twist, queue_size=5)
 
@@ -143,8 +142,7 @@ class GazeboCircuit2TurtlebotLidarDdpgEnv(gazebo_env.GazeboEnv):
         else:
             reward = -5
 
-        #currentlly disabled
-        #reward += dist_reward
+        reward += dist_reward
 
         # Check goal state
         if dist < 0.5:
